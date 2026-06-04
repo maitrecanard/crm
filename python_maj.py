5830ce2e784faaf9acec0796b628ddccc2caaaa7
Mathieu Siaudeau <44211309+maitrecanard@users.noreply.github.com>
2026-06-04 22:46:32 +0200

    Create python_maj.py

---
+++ python_maj.py
@@ -0,0 +1,4 @@
+Met à jour le projet python avec les valeurs suivantes, puis supprime ce fichier :
+export CRM_URL=https://crm.techcaresolutions.fr
+export CRM_TOKEN=1|pmRRj7W3mQfThEcJTrHuAJSSCNW4l3UkmAi4k9bGe7d8def0
+python3 push_to_crm.py   
